798fa04bbf87465db09228724baca9b95e7dfdbe
jefripunza <35704193+jefripunza@users.noreply.github.com>
2020-11-16 17:28:33 +0700

    Add files via upload

---
+++ lib/config___settings.php
@@ -0,0 +1,17 @@
+<?php
+// ICEcoder system settings
+$ICEcoderSettings = array(
+    "versionNo" => "1.0",
+    "codeMirrorDir" => "CodeMirror",
+    "docRoot" => "/data/data/com.termux/files/home/ngodingyuk",
+    "demoMode" => false,
+    "devMode" => false,
+    "fileDirResOutput" => "none", // Can be none, raw, object, both (all but 'none' output to console)
+    "loginRequired" => true,
+    "multiUser" => false,
+    "languageBase" => "english.php",
+    "lineEnding" => "\n",
+    "newDirPerms" => 755,
+    "newFilePerms" => 644,
+    "enableRegistration" => true,
+);
